d655363e62daed9bb4faa11a610401c92f7b6258
임성훈 <sasumpi123@naver.com>
2020-04-13 17:16:42 +0900

    Blog init

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `MuriMuri's Dev Note`,
+  description: `Blog posted about ...`,
+  author: `[MuriMuri]`,
+  introduction: `무리무리의 DevNote..\n 공부한 것을 기록하자...!`,
+  siteUrl: `https://sasumpi123.github.io/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `sasumpi123`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'sasumpi123/sasumpi123.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
