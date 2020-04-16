0267cccea31e05ab0bcf87cf8aa81ead0a26da44
임성훈 <sasumpi123@naver.com>
2020-04-16 18:08:32 +0900

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: metaConfig.ga,
+        trackingId: 'UA-163753757-1',
       },
     },
     {
